bcb6497e60f9b3b4ea6ae9bdbeb84c27c41f67ca
yi.luo <yi.luo@advantech.com.cn>
2020-02-11 11:45:39 +0800

    层级概览代码优化

--- MPMProject/Controllers/LevelsOverviewController.cs
+++ MPMProject/Controllers/LevelsOverviewController.cs
@@ -19,8 +19,6 @@ namespace MPMProject.Controllers
     public class LevelsOverviewController : BaseController
     {
         public string url = "http://api-mpm.wise-paas.cn/";
-        //全局变量用于保存数据
-        public List<NavTree> treeNodes = new List<NavTree>();
 
         public IActionResult Index()
         {
@@ -29,43 +27,39 @@ namespace MPMProject.Controllers
 
         public JsonResult GetData()
         {
+            //由于设备为末节点，将其id全部设为-1
+            int id = -1;
+            //根节点
+            List<object> final = new List<object>() { new { name = "根节点", id = 0, upper_id = "-" } };
+
             //配置的节点
             var areaNodeUrl = url + "api/v1/configuration/public/area_node";
             var resAreaNode = GetUrl(areaNodeUrl);
-            JObject jo1 = (JObject)JsonConvert.DeserializeObject(resAreaNode);
-            var areaNodeList = jo1["data"].ToObject<IList<Model.area_node>>();
+            JObject joAreaNode = (JObject)JsonConvert.DeserializeObject(resAreaNode);
 
             //绑定的设备
             var machineUrl = url + "api/v1/configuration/public/machine";
             var resMachine = GetUrl(machineUrl);
-            JObject jo = (JObject)JsonConvert.DeserializeObject(resMachine);
-            var machineList = jo["data"].ToObject<IList<Model.machine>>();
-  
-            int id = -1;
-            var datAreaNode = (from p in areaNodeList
-                       select new { name = p.name_cn, p.id,  p.upper_id }).ToList();
-            var datMachine = (from q in machineList
-                        select new { name = q.name_cn, id,  upper_id = q.area_node_id }).ToList();
-            List<object> final=new List<object>() {
-                new { name="根节点",id=0,upper_id="-"}
-                };
-            var dat = final.Union(datAreaNode).Union(datMachine).ToList();
-            switch (Convert.ToInt32(jo["code"]))
-            {
-                case 200:
-                    Json(dat);
-                    break;
-                case 400:
-                    break;
-                case 410:
-                    break;
-                case 411:
-                    break;
-                default:
-                    break;
-            }
+            JObject joMachine = (JObject)JsonConvert.DeserializeObject(resMachine);
 
-            return Json(dat);
+            if (Convert.ToInt32(joAreaNode["code"]) == 200 && Convert.ToInt32(joMachine["code"]) == 200)
+            {
+                var areaNodeList = joAreaNode["data"].ToObject<IList<Model.area_node>>();
+                var machineList = joMachine["data"].ToObject<IList<Model.machine>>();
+                //层级
+                var datAreaNode = (from p in areaNodeList
+                                   select new { name = p.name_cn, p.id, p.upper_id }).ToList();
+                //设备
+                var datMachine = (from q in machineList
+                                  select new { name = q.name_cn, id, upper_id = q.area_node_id }).ToList();
+                var dat = final.Union(datAreaNode).Union(datMachine).ToList();
+                return Json(dat);
+            }
+            else
+            {
+                return Json(final);
+            }
+            
         }
 
     }
