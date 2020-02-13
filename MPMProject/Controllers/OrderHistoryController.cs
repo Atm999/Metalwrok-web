ff02f2e4d33c956cfaf8ee620a0330806862fe61
Abby.Liu <Abby.Liu@A-100022128>
2020-02-13 20:48:18 +0800

    update

--- MPMProject/Controllers/OrderHistoryController.cs
+++ MPMProject/Controllers/OrderHistoryController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MPMProject.Controllers
 {
     public class OrderHistoryController : BaseController
     {
+        public string url = "http://api-mpm.wise-paas.cn/";
+
         public IActionResult Index()
         {
             return View();
+        }
+        public JsonResult GetData()
+        {
+            url = url + "api/v1/configuration/work_order/produced_work_order";
+            string result = GetUrl(url);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(result);
+            switch (Convert.ToInt32(jo["code"]))
+            {
+                case 200:
+                    Json(jo["data"]);
+                    break;
+                case 400:
+                    break;
+                case 410:
+                    break;
+                case 411:
+                    break;
+                default:
+                    break;
+            }
+            return Json(jo["data"]);
         }
     }
 }
