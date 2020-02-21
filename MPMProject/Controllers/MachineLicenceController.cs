3dd813cfec765610912f3f5d9aaf8850c0e0f99b
yi.luo <yi.luo@advantech.com.cn>
2020-02-21 15:14:42 +0800

    设备数量licence授权

---
+++ MPMProject/Controllers/MachineLicenceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Model.FileEncode;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MPMProject.Controllers
+{
+    [EnableCors("AllowSpecificOrigin")]
+    public class MachineLicenceController : BaseController
+    {
+        private IHostingEnvironment hostingEnv;
+        public MachineLicenceController(IHostingEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+        public IActionResult Index()
+        {
+            string data1=DESCode.Read(hostingEnv.WebRootPath+ $@"\Files\Files\licence.txt");
+            string data = "{\"space_id\":\"0e8d66cd-55e6-4853-9835-2b24e6a95022\",\"machineNum\":100}";
+            string key = "WisePaaS";
+            string iv = "AKTCKM30";
+            ViewBag.Encrypt = DESCode.DESEncrypt(data, key, iv);
+
+            string licence = DESCode.DESDecrypt(data1, key, iv);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(licence);
+            ViewBag.licenceNum = jo["machineNum"].ToString();
+            return View();
+        }
+
+        public IActionResult Decode()
+        {
+            return View();
+        }
+    }
+}
---
+++ Model/FileEncode/DESCode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Model.FileEncode
+{
+    public class DESCode
+    {
+        //读取文件
+        public static string Read(string path)
+        {
+            string conStr = "";
+            try
+            {
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                StreamReader reader = new StreamReader(fs);
+                conStr = reader.ReadLine();
+                fs.Close();
+            }
+            catch (Exception)
+            {
+                conStr = "";
+            }
+            return conStr;
+        }
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="data">加密数据</param>
+        /// <param name="key">8位字符的密钥字符串</param>
+        /// <param name="iv">8位字符的初始化向量字符串</param>
+        /// <returns></returns>
+        public static string DESEncrypt(string data, string key, string iv)
+        {
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(iv);
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            int i = cryptoProvider.KeySize;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, byIV), CryptoStreamMode.Write);
+
+            StreamWriter sw = new StreamWriter(cst);
+            sw.Write(data);
+            sw.Flush();
+            cst.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="data">解密数据</param>
+        /// <param name="key">8位字符的密钥字符串(需要和加密时相同)</param>
+        /// <param name="iv">8位字符的初始化向量字符串(需要和加密时相同)</param>
+        /// <returns></returns>
+        public static string DESDecrypt(string data, string key, string iv)
+        {
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(iv);
+
+            byte[] byEnc;
+            try
+            {
+                byEnc = Convert.FromBase64String(data);
+            }
+            catch
+            {
+                return null;
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream ms = new MemoryStream(byEnc);
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateDecryptor(byKey, byIV), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cst);
+            return sr.ReadToEnd();
+        }
+
+    }
+}
