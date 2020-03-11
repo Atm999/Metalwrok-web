6a21a83dee7d950a6f07550b14627cfc5d2c7962
yi.luo <yi.luo@advantech.com.cn>
2020-03-11 11:06:56 +0800

    bug20200311

---
+++ MPMProject/Controllers/OnsiteMachineStatusController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MPMProject.Controllers
+{
+    public class OnsiteMachineStatusController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Json("连接成功");
+        }
+
+        [HttpPost]
+        public IActionResult Search()
+        {
+            return Json("连接成功");
+        }
+
+        [HttpPost]
+        public IActionResult Query()
+        {
+            return Json("连接成功");
+        }
+
+
+    }
+}
