12554f5aad6da856ee066f6a1c6bdf9de18f8cc5
abby.liu <abby.liu@advantech.com.cn>
2020-06-12 11:36:35 +0800

    新增环境

---
+++ MPMProject/Controllers/StandardController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MPMProject.Controllers
+{
+    /// <summary>
+    ///环境
+    /// </summary>
+    public class StandardController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetData()
+        {
+            var purl = url + "api/v1/configuration/ehs/env_standard";
+
+            var subList = CommonHelper<env_standard>.Get(purl, HttpContext);
+
+            return Json(subList);
+        }
+        /// <summary>
+        /// 类别
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetTagType()
+        {
+            var purl = url + "api/v1/configuration/public/tag_type";
+            string myurl = url + "api/v1/configuration/public/tag_type_sub";
+
+            var typeList = CommonHelper<tag_type>.Get(purl, HttpContext);
+            var subList = CommonHelper<tag_type_sub>.Get(myurl, HttpContext);
+            var list = typeList.Where(p => p.name_cn == "环境");
+            var jo2 = subList.Join(list, p => p.tag_type_id, p => (p as Model.tag_type).id, (p, q) => new { p.id, p.name_cn, p.name_en, p.name_tw, typeName = q.name_cn, p.tag_type_id, p.description }).ToList();
+            return Json(jo2);
+        }
+        /// <summary>
+        /// 标签名
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetTaginfo()
+        {
+            var purl = url + "api/v1/configuration/public/tag";
+            return Json(CommonHelper<tag_info>.Get(purl, HttpContext));
+        }
+        public JsonResult Getarea()
+        {
+            var purl = url + "api/v1/configuration/public/area_node";
+            return Json(CommonHelper<area_node>.Get(purl, HttpContext));
+        }
+        public IActionResult Update([FromBody]standard sub)
+        {
+            string msg = "";
+            string myurl = url + "api/v1/configuration/ehs/env_standard";
+            string postData = JsonConvert.SerializeObject(sub);
+            string result = PutUrl(myurl, postData);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(result);
+            switch (Convert.ToInt32(jo["code"]))
+            {
+                case 200:
+                    msg = "Success";
+                    break;
+                case 400:
+                    msg = "fail";
+                    break;
+            }
+            return Json(msg);
+        }
+        public IActionResult Add([FromBody]standard sub)
+        {
+            string msg = "";
+
+            string myurl = url + "api/v1/configuration/ehs/env_standard";
+            var postData = JsonConvert.SerializeObject(sub);
+            string result = PostUrl(myurl, postData);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(result);
+            switch (Convert.ToInt32(jo["code"]))
+            {
+                case 200:
+                    msg = "Success";
+                    break;
+                case 400:
+                    msg = "fail";
+                    break;
+            }
+            return Json(msg);
+
+        }
+
+        public IActionResult Delete([FromBody]standard sta)
+        {
+            string msg = "";
+            string myurl = url + "api/v1/configuration/ehs/env_standard?id=" + sta.id.ToString();
+            string result = DeleteUrl(myurl);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(result);
+            switch (Convert.ToInt32(jo["code"]))
+            {
+                case 200:
+                    msg = "Success";
+                    break;
+                case 400:
+                    msg = "fail";
+                    break;
+            }
+            return Json(msg);
+        }
+    }
+}
---
+++ Model/standard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Model
+{
+    public class standard 
+    {
+        public int id { get; set; }
+        /// <summary>
+        /// 标签id
+        /// </summary>
+        public int tag_id { get; set; } 
+        /// <summary>
+        /// 
+        /// </summary>
+        public int  tag_type_sub_id { get; set; }
+        /// <summary>
+        /// 正常范围最小值
+        /// </summary>
+        public double? normal_min { get; set; }
+        /// <summary>
+        /// 正常范围最大值
+        /// </summary>
+        public double? normal_max { get; set; }
+        /// <summary>
+        /// 异常范围最小值
+        /// </summary>
+        public double? abnormal_min { get; set; }
+        /// <summary>
+        /// 异常范围最大值
+        /// </summary>
+        public double? abnormal_max { get; set; }
+        /// <summary>
+        /// 严重范围最小值
+        /// </summary>
+        public double? serious_min { get; set; }
+        /// <summary>
+        /// 严重范围最大值
+        /// </summary>
+        public double? serious_max { get; set; }
+    }
+
+    public class env_standard : standard
+    {
+        public tag_type_sub tag_type_sub { get; set; }
+        public area_node area_node { get; set; }
+        public area_layer area_layer { get; set; }
+        public machine machine { get; set; }
+        public tag_info tag_info { get; set; }
+    }
+}
