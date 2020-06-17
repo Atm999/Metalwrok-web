6a13e773f836fbbdc2e7b262135e9df3298e1a22
abby.liu <abby.liu@advantech.com.cn>
2020-06-17 17:05:15 +0800

    新增通知逻辑

---
+++ MPMProject/Controllers/NotificationController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MPMProject.Controllers
+{
+    public class NotificationController : BaseController
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetData()
+        {
+            string myurl = url + "api/v1/configuration/ehs/notice_logic";
+            List<notice_logics> list = CommonHelper<notice_logics>.Get(myurl, HttpContext);
+            list = list.OrderByDescending(x => x.id).ToList();
+            return Json(list);
+        }
+
+
+        public IActionResult Update([FromBody]notice_logic ec)
+        {
+            string msg = "";
+            string myurl = url + "api/v1/configuration/ehs/notice_logic";
+            var postData = JsonConvert.SerializeObject(ec);
+            string result = PutUrl(myurl, postData);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(result);
+            switch (Convert.ToInt32(jo["code"]))
+            {
+                case 200:
+                    msg = "Success";
+                    break;
+                case 400:
+                    msg = "fail";
+                    break;
+
+            }
+            return Json(msg);
+        }
+        public IActionResult Add([FromBody]notice_logic ec)
+        {
+            string msg = "";
+            string myurl = url + "api/v1/configuration/ehs/notice_logic";
+            var postData = JsonConvert.SerializeObject(ec);
+            string result = PostUrl(myurl, postData);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(result);
+            switch (Convert.ToInt32(jo["code"]))
+            {
+                case 200:
+                    msg = "Success";
+                    break;
+                case 400:
+                    msg = "fail";
+                    break;
+
+            }
+
+            return Json(msg);
+        }
+
+        public IActionResult Delete([FromBody]notice_logic ec)
+        {
+            string myurl = url + "api/v1/configuration/ehs/notice_logic?id=" + ec.id.ToString();
+            string result = DeleteUrl(myurl);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(result);
+            switch (Convert.ToInt32(jo["code"]))
+            {
+                case 200:
+                    Json("Success");
+                    break;
+                case 400:
+                    break;
+                case 410:
+                    break;
+                case 411:
+                    break;
+                default:
+                    break;
+            }
+            return Json("Success");
+        }
+
+        public JsonResult Getgroup()
+        {
+            string myurl = url + "api/v1/configuration/andon/notification_group";
+
+            return Json(CommonHelper<notification_group>.Get(myurl, HttpContext));
+        }
+    }
+}
---
+++ Model/notice_logic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class notice_logic
+    {
+        public int id { set; get; }
+        /// <summary>
+        /// 逻辑名
+        /// </summary>
+        public string name { set; get; }
+        public int normal_notification_group_id { get; set; }
+        /// <summary>
+        /// 二级通知人员id
+        /// </summary>
+        public int abnormal_notification_group_id { get; set; }
+        /// <summary>
+        /// 三级通知人员id
+        /// </summary>
+        public int serious_notification_group_id { get; set; }
+        /// <summary>
+        /// 预警形式，邮件  微信
+        /// </summary>
+        public int notice_type { get; set; }
+    }
+
+    public class notice_logics : notice_logic
+    {
+        public notification_group normal_notification_group { get; set; } 
+        public notification_group abnormal_notification_group { get; set; }
+        public notification_group serious_notification_group { get; set; } 
+
+    }
+   
+}
